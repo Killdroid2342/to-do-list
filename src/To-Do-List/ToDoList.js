3c20a9c2ce678892373bcabf23b778bad66989b6
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-10-23 22:54:33 +0100

    "added header and list section"

--- src/To-Do-List/ToDoList.js
+++ src/To-Do-List/ToDoList.js
@@ -2,21 +2,27 @@ import React from 'react';
 
 const ToDoList = () => {
   return (
-    <div className='center'>
-      <div className='border-2 border-purple-900 w-96 h-96 rounded-lg'>
-        <h1 className='text-center mt-4 text-xl font-bold text-white'>
-          To Do List
-        </h1>
-        <form className='mb-10 mt-10'>
-          <label className='text-white mr-5 ml-5'>Add Task</label>
-          <input className='rounded-lg' type='text' />
-        </form>
-        <div className='center flex-col'>
-          <button className='btn mb-4 '>Add To List</button>
-          <button className='btn'>Remove To List</button>
+    <React.Fragment>
+      <h1 className='center mt-4 text-4xl	font-bold text-white mb-8'>
+        To Do List
+      </h1>
+      <div className='center'>
+        <div className='border-2 border-purple-900 w-96 max-h-screen rounded-lg'>
+          <h2 className='header'>Add List</h2>
+          <form className='mb-10 mt-10'>
+            <label className='text-white mr-5 ml-5'>Add Task</label>
+            <input className='rounded-lg' type='text' />
+          </form>
+          <div className='center flex-col'>
+            <button className='btn mb-4 '>Add To List</button>
+            <button className='btn'>Remove To List</button>
+          </div>
+          <div className='border-2 border-purple-900 h-96 rounded-lg mt-5'>
+            <h2 className='header'>List</h2>
+          </div>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
