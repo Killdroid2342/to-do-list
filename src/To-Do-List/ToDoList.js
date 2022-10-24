2ced7bce7822b8c423a58e5f49dfd4a704d36468
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-10-24 01:48:42 +0100

    "added some state and functionality"

--- src/To-Do-List/ToDoList.js
+++ src/To-Do-List/ToDoList.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ToDoList = () => {
+const ToDoList = (props) => {
+  const [input, setInput] = useState('');
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+    setInput('');
+  };
+
   return (
     <React.Fragment>
       <h1 className='center mt-4 text-4xl	font-bold text-white mb-8'>
@@ -9,9 +25,14 @@ const ToDoList = () => {
       <div className='center'>
         <div className='border-2 border-purple-900 w-96 max-h-screen rounded-lg'>
           <h2 className='header'>Add List</h2>
-          <form className='mb-10 mt-10'>
+          <form className='mb-10 mt-10' onSubmit={handleSubmit}>
             <label className='text-white mr-5 ml-5'>Add Task</label>
-            <input className='rounded-lg' type='text' />
+            <input
+              className='rounded-lg'
+              type='text'
+              value={input}
+              onChange={handleChange}
+            />
           </form>
           <div className='center flex-col'>
             <button className='btn mb-4 '>Add To List</button>
