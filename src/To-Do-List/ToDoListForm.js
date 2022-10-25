a8545eccf036c8fe3229ae97beed6ab3037b6abc
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-10-25 22:05:50 +0100

    "made text appear on page under list not finished"

--- src/To-Do-List/ToDoListForm.js
+++ src/To-Do-List/ToDoListForm.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ToDoListForm = () => {
-  const test = (e) => {
-    console.log(e.target.value);
-  };
+  const [testing, setTesting] = useState('');
+
+  // const test = (e) => {
+  //   console.log(e.target.value);
+  // };
+  console.log(testing);
 
   return (
     <React.Fragment>
@@ -15,7 +18,12 @@ const ToDoListForm = () => {
           <h2 className='header'>Add List</h2>
           <form className='mb-10 mt-10'>
             <label className='text-white mr-5 ml-5'>Add Task</label>
-            <input className='rounded-lg' type='text' onChange={test} />
+            <input
+              className='rounded-lg'
+              type='text'
+              value={testing}
+              onChange={(e) => setTesting(e.target.value)}
+            />
           </form>
           <div className='center flex-col'>
             <button className='btn mb-4'>Add To List</button>
@@ -23,6 +31,7 @@ const ToDoListForm = () => {
           </div>
           <div className='border-2 border-purple-900 h-96 rounded-lg mt-5'>
             <h2 className='header'>List</h2>
+            <p>{testing}</p>
           </div>
         </div>
       </div>
