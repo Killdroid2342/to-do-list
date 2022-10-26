483f3061b6f17e46d1acb9c83075cf2b2462c142
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-10-26 20:57:52 +0100

    "re do logic. last logic i couldnt add items"

--- src/To-Do-List/ToDoListForm.js
+++ src/To-Do-List/ToDoListForm.js
@@ -1,17 +1,7 @@
 import React, { useState } from 'react';
 
 const ToDoListForm = () => {
-  const [testing, setTesting] = useState('');
-
-  // console.log(testing);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = { testing };
-    // console.log('HELLO THIS IS A TEST');
-    console.log(data);
-  };
-
+  const [items, setItems] = useState([]);
   return (
     <React.Fragment>
       <h1 className='center mt-4 text-4xl	font-bold text-white mb-8'>
@@ -22,24 +12,14 @@ const ToDoListForm = () => {
           <h2 className='header'>Add List</h2>
           <form className='mb-10 mt-10'>
             <label className='text-white mr-5 ml-5'>Add Task</label>
-            <input
-              className='rounded-lg'
-              type='text'
-              value={testing}
-              onChange={(e) => setTesting(e.target.value)}
-            />
+            <input className='rounded-lg' type='text' />
           </form>
           <div className='center flex-col'>
-            <button className='btn mb-4' onClick={handleSubmit}>
-              Add To List
-            </button>
+            <button className='btn mb-4'>Add To List</button>
             <button className='btn'>Remove To List</button>
           </div>
           <div className='border-2 border-purple-900 h-96 rounded-lg mt-5'>
             <h2 className='header'>List</h2>
-            <p className='center mt-4 text-1xl border-2 border-black bg-purple-900 font-bold text-white'>
-              {testing}
-            </p>
           </div>
         </div>
       </div>
