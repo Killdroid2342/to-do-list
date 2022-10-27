f529e37af3c0d2842eb287173d32ba5f20f5b72d
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-10-27 18:27:14 +0100

    "added useState and useRef"

--- src/To-Do-List/ToDoListForm.js
+++ src/To-Do-List/ToDoListForm.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 const ToDoListForm = () => {
+  const inRef = useRef();
+  const [add, setAdd] = useState([]);
+
   return <div></div>;
 };
 
