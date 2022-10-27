eb14114a8b264296f233185b8dd9b7c13634a5b0
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-10-27 21:59:59 +0100

    "simple styling"

--- src/To-Do-List/ToDoListForm.js
+++ src/To-Do-List/ToDoListForm.js
@@ -17,14 +17,19 @@ const ToDoListForm = () => {
   };
   return (
     <React.Fragment>
-      <form onSubmit={click}>
-        add: <input ref={inRef} type='text'></input>
-        <button type='submit'>Add Tasks</button>
-      </form>
-      <h2>Tasks</h2>
-      {add.map((adds) => (
-        <div>{adds}</div>
-      ))}
+      <div className='border-2 border-purple-900 flex flex-col items-center justify-center '>
+        <form onSubmit={click}>
+          <h1 className='text-white ml-14'>To Do List</h1>
+          <input ref={inRef} type='text'></input>
+          <button type='submit' className='ml-10 text-white bg-purple-900 p-2'>
+            Add Tasks
+          </button>
+        </form>
+        <h2 className=''>Tasks</h2>
+        {add.map((adds) => (
+          <li>{adds}</li>
+        ))}
+      </div>
     </React.Fragment>
   );
 };
