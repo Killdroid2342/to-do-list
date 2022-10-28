f475f55012879625db9b52c66e012b0c192b080c
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-10-28 23:24:48 +0100

    "fixed positioning"

--- src/To-Do-List/ToDoListForm.js
+++ src/To-Do-List/ToDoListForm.js
@@ -21,7 +21,7 @@ const ToDoListForm = () => {
     <React.Fragment>
       <div className='border-2 border-purple-900 flex flex-col items-center justify-center '>
         <form onSubmit={click}>
-          <h1 className='header'>To Do List</h1>
+          <h1 className='header text-center'>To Do List</h1>
           <div className='border-2 border-purple-900 flex flex-col items-center justify-center mb-4'>
             <p className='text-white mb-3'>Enter Task Below</p>
             <input ref={inRef} type='text' className='mb-3'></input>
